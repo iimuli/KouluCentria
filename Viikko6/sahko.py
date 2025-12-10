6224201eb5a4c21098e8e9c2dec56820ac2658d0
Eemeli Väisänen <noka567@hotmail.com>
2025-12-10 12:55:17 +0200

    viikko6valmis

--- Viikko6/sahko.py
+++ Viikko6/sahko.py
@@ -1,9 +1,8 @@
 # Copyright (c) 2025 Juha Eemeli Väisänen
 # License: MIT
 
-from enum import IntEnum
-from datetime import datetime, date, time, timedelta
-
+from enum import IntEnum, Enum
+from datetime import datetime, timedelta
 
 class EData(IntEnum): 
     """ihmisystävällisempi tiedon haku listoilta """
@@ -11,18 +10,39 @@ class EData(IntEnum):
     netPro= 1
     avgTemp = 2
 
+class FMonth(Enum):
+    """Muunnos suomalaiseen kuukauden nimeen numerolla"""
+    Tammikuu = 1
+    Helmikuu = 2
+    Maaliskuu = 3
+    Huhtikuu = 4
+    Toukokuu = 5
+    Kesäkuu = 6
+    Heinäkuu = 7
+    Elokuu = 8
+    Syyskuu = 9
+    Lokakuu = 10
+    Marraskuu = 11
+    Joulukuu = 12
+    @classmethod
+    def monthFromNumber(cls, monthNumber):
+        return cls(monthNumber).name.capitalize()
 
-# Aika;Kulutus (netotettu) kWh;Tuotanto (netotettu) kWh;Vuorokauden keskilämpötila
-# 2025-01-01   T00:00:00.000+02:00  == h/m/s/ms/timezone;      1,569;      0,000;     -4,5
+csvFile = None
 
 def main():
-    
-    csvFile = '2025.csv'
-    data2025 = readCSV(csvFile)
-    inputPromptLoop(data2025)
+    """Main, tekee csvFilest(kirjasto) globaalin, jos tiedost on null/None, hakee datan"""
+    global csvFile
+
+    if csvFile == None:
+        print('getting csv')
+        csvFile = readCSV('2025.csv')
+
+    inputPromptLoop(csvFile)
 
 
 def inputPromptLoop(csv):
+    """Selkeet inputit eri vaihtoehdoille while loopis"""
 
     print('\n\n\nValitse raporttityyppi:\n'
     '1) Päiväkohtainen yhteenveto aikaväliltä\n'
@@ -35,19 +55,19 @@ def inputPromptLoop(csv):
 
         if prompt == '1':
             raportByDate(csv)
+            continue
         elif prompt == '2':
             monthRaport(csv)
+            continue
         elif prompt == '3':
             yearRaport(csv)
+            continue
         elif prompt == '4':
-            print('end')
-            return
-
-
-
-    ##return None
+            print('Hei hei!')
+            exit()
 
 def raportByDate(csv):
+    """Inputit aikavälille, hakee kaikki datat kyseiseltä aikaväliltä (käyttö, tuotto, keskimääräinen lämpötila)"""
 
     earliestDate = next(iter(csv))
     latestdate = next(reversed(csv))
@@ -74,43 +94,102 @@ def raportByDate(csv):
         except ValueError:
             print("Väärä inputti")
     
-    days = (date2 - date1).days
+    days = (date2 - date1).days + 1
 
     overAllConsumption = 0
     overAllProduction = 0
     tempSum = 0
 
-    print('-------------------------------------------------------------------------------------------\n'
+    print(f'{'-' * 130}\n'
           f'Aikaväli: {ChangeToFinnishDate(date1)} - {ChangeToFinnishDate(date2)}.\n\n'
-          'Kokonaiskulutus [kWh]           Kokonaistuotanto [kWh]          Keskilämpötila [C]\n')
+          'Pvm [päivä/kuukausi/vuosi]           Kokonaiskulutus/vrk [kWh]           Kokonaistuotanto/vrk [kWh]          Keskilämpötila/vrk [°C]\n')
 
-    for i in range(days + 1):
+    for i in range(days):
 
         currentDate = date1 + timedelta(days=i)
         overAllProduction += csv[currentDate][EData.netPro]
         overAllConsumption += csv[currentDate][EData.netCon]
         tempSum += csv[currentDate][EData.avgTemp]
+        print(f'{currentDate}{' ' * 30}{csv[currentDate][EData.netCon]:.2f}{' ' * 30}{csv[currentDate][EData.netPro]:.2f}{' ' * 30}{csv[currentDate][EData.avgTemp]:.2f}')
 
-    print(f'{' ' * 7}{overAllConsumption:.2f}{' ' * 25}{overAllProduction:.2f}{' ' * 28}{(tempSum / days + 1):.2f}\n'
-          '-------------------------------------------------------------------------------------------\n\n')
+    print(f'\nKokonais kulutus aikavälillä [kWh]          Kokonais tuotto aikavälillä [kWh]        Keskilämpötila aikavälillä [°C]\n\n'
+          f'{' ' * 10}{overAllConsumption:.2f}{' ' * 40}{overAllProduction:.2f}{' ' * 40}{(tempSum / days):.2f}\n')
+    print(f'\n{'-' * 130}\n\n')
 
-    return ##left here
+
+    returnToMain()
 
 
 
 def monthRaport(csv):
-    print('motn raport')
-    return None
+    """numerolla valittu kuukauden data"""
+
+    print(f'\n\n\nValitse kuukausi: 1 - 12')
+    while True:
+        selectedMonth = input('Kuukausi (numero): ')
+        try:
+            if 1 <= int(selectedMonth) <= 12:
+                selectedMonth = int(selectedMonth)
+                break
+        except ValueError:
+            print("Väärä inputti")
+
+
+    overAllConsumption = 0
+    overAllProduction = 0
+    tempSum = 0
+    daysInMonth = 0
+
+    for date in csv.keys():
+        if date.month == selectedMonth:
+            daysInMonth += 1
+            overAllConsumption += csv[date][EData.netCon]
+            overAllProduction += csv[date][EData.netPro]
+            tempSum += csv[date][EData.avgTemp]
+
+    print(f'\n\n{'-' * 130}\n'
+          f'Valittu kuukausi: {FMonth.monthFromNumber(selectedMonth)}, päiviä kirjattu: {daysInMonth}\n\n'
+          'Kokonaiskulutus/vrk [kWh]           Kokonaistuotanto/vrk [kWh]          Keskilämpötila/vrk [°C]\n'
+          f'{' ' * 10}{overAllConsumption:.2f}{' ' * 30}{overAllProduction:.2f}{' ' * 30}{(tempSum / daysInMonth):.2f}\n'
+          f'{'-' * 130}\n\n')
+
+
+    returnToMain()
+    
+   
 
 def yearRaport(csv):
-    print('year raport')
-    return None
+    """kaikki vuoden data samaan pakettiin"""
+
+    overAllConsumption = 0
+    overAllProduction = 0
+    tempSum = 0
+    daysInMonth = 0
+
+
+    for d in csv.keys():
+        daysInMonth += 1
+        overAllConsumption += csv[d][EData.netCon]
+        overAllProduction += csv[d][EData.netPro]
+        tempSum += csv[d][EData.avgTemp]
+
+    print(f'\n\n{'-' * 130}\n'
+        f'Vuoden 2025 raportti:\n\n'
+        'Kokonaiskulutus/vrk [kWh]           Kokonaistuotanto/vrk [kWh]          Keskilämpötila/vrk [°C]\n'
+        f'{' ' * 10}{overAllConsumption:.2f}{' ' * 30}{overAllProduction:.2f}{' ' * 30}{(tempSum / daysInMonth):.2f}\n'
+        f'{'-' * 130}\n\n')
+
+
+    returnToMain()
 
 
 def readCSV(csv):
+    """lukee txt, jakaa kaikki omaan date avaimiin kirjastoon (data_date : vuosi, kuukausi, päivä),
+      ja tekee niille listan arvoista. Laskee valmiiksi yhteen käytön ja tuoton, sekä keskimääräisen lämpötilan per vrk"""
     dataMap = {}
     avgTemperature = []
-    dayIndex = 0
+    currentDate = None
+    appended = False
 
     with open(csv , "r", encoding="utf-8") as f: 
         next(f)
@@ -119,40 +198,51 @@ def readCSV(csv):
 
             data = line.strip().split(";")
             data_date = datetime.strptime(data[0].strip().split("T")[0], "%Y-%m-%d").date()
+            appended = False
 
-            ##data_time = data[0].strip().split("T")[1].strip().split(".")[0] ei tarvine kellonaikaa, kun päivä kerrallaan halutaa dataa?
-            ##data_list.append(str(data_time))
+            if data_date not in dataMap: 
 
+                if currentDate != None:
+                    tempAll = sum(avgTemperature) / len(avgTemperature)
+                    avgTemperature = []
+                    dataMap[currentDate].append(float(tempAll))
+                    appended = True
 
-            if data_date not in dataMap:
+                currentDate = data_date
                 data_list = []
                 data_list.append(float(data[1].replace(",",".")))
                 data_list.append(float(data[2].replace(",",".")))
 
                 avgTemperature.append(float(data[3].replace(",",".")))
-                dataMap[data_date] = data_list
-                dayIndex = 1
+                dataMap[currentDate] = data_list
+                
 
             else:
-                dayIndex += 1
-                dataMap[data_date][EData.netCon] += float(data[1].replace(",","."))
-                dataMap[data_date][EData.netPro] += float(data[2].replace(",","."))
-
-                """Näköjää lämpötila pysyy staattisena päivittäin, mutta tämä laskee vuorokauden keskimääräisen lämpötilan"""
+                dataMap[currentDate][EData.netCon] += float(data[1].replace(",","."))
+                dataMap[currentDate][EData.netPro] += float(data[2].replace(",","."))
                 avgTemperature.append(float(data[3].replace(",",".")))
-                if dayIndex == 24:
-                    dayIndex = 0
-                    tempAll = 0
-                    for temp in avgTemperature:
-                        tempAll += temp
-                    tempAll = sum(avgTemperature) / len(avgTemperature)
-                    avgTemperature = []  
-                    data_list.append(float(tempAll))
+
+    if appended == False: ##ylijäämä lisäys = jos kyseessä viimeinen haettu päivämäärä
+        tempAll = sum(avgTemperature) / len(avgTemperature)
+        dataMap[currentDate].append(float(tempAll))
 
     return dataMap
 
 def ChangeToFinnishDate(date):
+     """xxxx.xx.xx -> xx.xx.xxxx"""
      return date.strftime("%d.%m.%Y")
+
+def returnToMain():
+    """joka funktion lopussa q inputil valikkoon takaisin, antaa käyttäjälle aikaa perehtyä dataan."""
+    print('Paina "q" palataksesi valikkoon.')
+
+    while True:
+        returnKey = input()
+        try:
+            if returnKey == 'q':
+                main()
+        except ValueError:
+            None
 
 if __name__ == "__main__":
     main()
